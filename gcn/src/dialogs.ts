b46e61c9f3256fb0114b2eca5c2c7c2a97714dbd
Svata Dedic <svatopluk.dedic@oracle.com>
2022-12-05 17:56:30 +0100

    [GCN-1131] Report early audit execution as no opened folders warning.

--- gcn/src/dialogs.ts
+++ gcn/src/dialogs.ts
@@ -145,7 +145,7 @@ export async function selectDirectory(options?: string[], actionName?: string, t
 export async function selectFolder(actionName: string | undefined = undefined, hint: string | undefined = undefined, serviceFolder: boolean | null = true): Promise<gcnServices.FolderData | null | undefined> {
     const choices: QuickPickObject[] = [];
     const folderData = gcnServices.getFolderData();
-    for (const folder of folderData) {
+    for (const folder of folderData || []) {
         if (serviceFolder === null || serviceFolder && folder.services.length > 0 || !serviceFolder && folder.services.length === 0) {
             const choice = new QuickPickObject(folder.folder.name, undefined, undefined, folder);
             choices.push(choice);
