56ee18aeb5221f6ca43441d3a0b13eb9b2778f8c
hassan ketlas <hassan.ketlas@oracle.com>
2023-04-18 23:01:42 +0000

    feat: initialize browser directory

---
+++ gcn/src/browser/extension.ts
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2023, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+ */
+
+import * as vscode from 'vscode';
+import * as gcnProjectCreate from './gcnProjectCreate';
+import * as logUtils from './../logUtils';
+
+
+export function activate(context: vscode.ExtensionContext) {
+    logUtils.logInfo('[extension] Activating extension');
+
+	context.subscriptions.push(vscode.commands.registerCommand('gcn.createGcnProject', () => gcnProjectCreate.createProject(context)));
+
+    logUtils.logInfo('[extension] Extension successfully activated');
+}
