943b8e42d574095c550b38debc3deec99f9cd493
Dusan Balek <dusan.balek@oracle.com>
2023-08-16 14:30:01 +0200

    [GCN-2921] Provide code lens to open endpoint in browser, execute REST call from editor, and navigate to endpoints explorer.

---
+++ micronaut-tools/src/navigation/codeLens.ts
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2023, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+ */
+
+import * as vscode from 'vscode';
+import * as actions from './actions';
+import * as restQueries from './restQueries';
+import * as symbols from './symbols';
+import * as views from './views';
+import * as logUtils from '../logUtils';
+
+export function initialize(context: vscode.ExtensionContext) {
+    context.subscriptions.push(vscode.languages.registerCodeLensProvider({ language: 'java' }, new CodeLensProvider()));
+}
+
+class CodeLensProvider implements vscode.CodeLensProvider {
+
+    private COMMAND_NBLS_DOCUMENT_SYMBOLS = 'nbls.document.symbols';
+    private _onDidChangeCodeLenses: vscode.EventEmitter<void> = new vscode.EventEmitter<void>();
+    onDidChangeCodeLenses: vscode.Event<void> | undefined = this._onDidChangeCodeLenses.event;
+
+    constructor() {
+        symbols.onUpdated((kind) => {
+            if (symbols.isEndpointKind(kind)) {
+                this._onDidChangeCodeLenses.fire();
+            }
+        });
+    }
+
+    async provideCodeLenses(document: vscode.TextDocument, _token: vscode.CancellationToken): Promise<vscode.CodeLens[]> {
+        const lenses = [];
+        const endpoints = await this.readDocumentEndpoints(document.uri);
+        if (endpoints) {
+            for (const endpoint of endpoints) {
+                const range = new vscode.Range(endpoint.startPos, endpoint.endPos);
+                lenses.push(new vscode.CodeLens(range, {
+                    title: actions.COMMAND_NAME_OPEN_IN_BROWSER,
+                    command: actions.COMMAND_OPEN_IN_BROWSER,
+                    arguments: [ endpoint ]
+                }));
+                lenses.push(new vscode.CodeLens(range, {
+                    title: restQueries.COMMAND_NAME_COMPOSE_REST_QUERY,
+                    command: restQueries.COMMAND_COMPOSE_REST_QUERY,
+                    arguments: [ endpoint ]
+                }));
+                lenses.push(new vscode.CodeLens(range, {
+                    title: views.COMMAND_NAME_REVEAL_IN_ENDPOINTS,
+                    command: views.COMMAND_REVEAL_IN_ENDPOINTS,
+                    arguments: [ endpoint ]
+                }));
+            }
+        }
+        return lenses;
+    }
+
+    private async readDocumentEndpoints(uri: vscode.Uri): Promise<symbols.Endpoint[]> {
+        const newEndpoints: symbols.Endpoint[] = [];
+        try {
+            if ((await vscode.commands.getCommands()).find(cmd => cmd === this.COMMAND_NBLS_DOCUMENT_SYMBOLS)) {
+                const endpoints: any[] = await vscode.commands.executeCommand(this.COMMAND_NBLS_DOCUMENT_SYMBOLS, uri.toString(), symbols.PREFIX_ENDPOINTS);
+                for (const endpoint of endpoints) {
+                    try {
+                        const name: string = endpoint.name;
+                        const startPos: vscode.Position = new vscode.Position(endpoint.selectionRange?.start?.line, endpoint.selectionRange?.start?.character);
+                        const endPos: vscode.Position = new vscode.Position(endpoint.selectionRange?.end?.line, endpoint.selectionRange?.end?.character);
+                        newEndpoints.push(new symbols.Endpoint(name, uri, startPos, endPos));
+                    } catch (err) {
+                        logUtils.logWarning(`[symbols] readDocumentEndpoints - failed to read endpoint: ${err}`);
+                    }
+                }
+            }
+        } catch (err) {
+            logUtils.logError(`[symbols] readDocumentEndpoints - failed to read endpoints: ${err}`);
+        }
+        newEndpoints.sort((o1, o2) => o1.def.localeCompare(o2.def));
+        return newEndpoints;
+    }
+}
