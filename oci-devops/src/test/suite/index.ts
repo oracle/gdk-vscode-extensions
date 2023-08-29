39ef7635d92a9920482d84c5a5402d1d55902fe9
Ondřej Douda <ondrej.douda@oracle.com>
2023-08-29 16:59:34 +0200

    Opensourcing fixes from suggestions.

--- oci-devops/src/test/suite/index.ts
+++ oci-devops/src/test/suite/index.ts
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2023, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+ */
+
 import * as path from 'path';
 import * as Mocha from 'mocha';
 import * as glob from 'glob';
