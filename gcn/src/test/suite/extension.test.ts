b6332f979fa66d60a388579baf14654c28b386ff
Ondřej Man <ondrejman1@gmail.com>
2023-09-27 11:20:00 +0200

    [GCN-3470] Add codelense tests and format other GCN tests

--- gcn/src/test/suite/extension.test.ts
+++ gcn/src/test/suite/extension.test.ts
@@ -22,7 +22,7 @@ import { NodeFileHandler } from '../../gcnProjectCreate';
  * @param search 
  * @returns 
  */
-function valueExists(arrayOfValues : Common.ValueAndLabel[], search : string) : boolean {
+function valueExists(arrayOfValues: Common.ValueAndLabel[], search: string): boolean {
         for (let arrayElem of arrayOfValues) {
                 if (arrayElem.value == search)
                         return true;
@@ -30,18 +30,21 @@ function valueExists(arrayOfValues : Common.ValueAndLabel[], search : string) : 
         return false;
 }
 
-suite('Extension Test Suite', function() {
-	vscode.window.showInformationMessage('Start all tests.');
+suite('Extension Test Suite', function () {
+        vscode.window.showInformationMessage('Start all tests.');
 
         // This test must be run first, in order to activate the extension (and wait for the activation to complete)
         test("Extension loaded", async () => {
+                // let java = vscode.extensions.getExtension("vscjava.vscode-java-pack");
+                // assert(java, "No java ext found");
+                // await java.activate();
+
                 let extension = vscode.extensions.getExtension('oracle-labs-graalvm.gcn');
                 assert(extension, "No GCN extension found!");
-
                 await extension.activate();
         });
         // Check if GCN commands have been loaded
-        test("GCN commands loaded", async () =>{
+        test("GCN commands loaded", async () => {
                 let commands = await vscode.commands.getCommands(true);
 
                 let containsGcnCommands = false;
@@ -57,8 +60,8 @@ suite('Extension Test Suite', function() {
         // configuration for creating a project
         let options = {
                 micronautVersion: {
-                  label: "3.7.4",
-                  serviceUrl: "",
+                        label: "3.7.4",
+                        serviceUrl: "",
                 },
                 applicationType: "APPLICATION",
                 buildTool: "GRADLE",
@@ -68,7 +71,7 @@ suite('Extension Test Suite', function() {
                 projectName: "demo",
                 javaVersion: "JDK_17",
                 clouds: [
-                  "OCI",
+                        "OCI",
                 ],
                 services: undefined,
                 features: undefined,
@@ -77,39 +80,39 @@ suite('Extension Test Suite', function() {
         test("Create GCN project - Project options", async () => {
                 // init gcnAPI
                 await Common.initialize();
-        
+
                 let micronautVersions = Common.getMicronautVersions();
                 assert.ok(micronautVersions.length, "No micronaut versions found");
 
                 let applicationTypes = await Common.getApplicationTypes();
                 assert.ok(applicationTypes.length, "No application type found");
-                assert.ok( valueExists(applicationTypes, "APPLICATION") );
+                assert.ok(valueExists(applicationTypes, "APPLICATION"));
 
                 let buildTools = Common.getBuildTools();
                 assert.ok(buildTools.length, "No build tools found");
-                assert.ok( valueExists(buildTools, "GRADLE") );
+                assert.ok(valueExists(buildTools, "GRADLE"));
 
                 let testFrameworks = Common.getTestFrameworks();
                 assert.ok(testFrameworks.length, "No test frameworks found");
-                assert.ok( valueExists(testFrameworks, "JUNIT") );
+                assert.ok(valueExists(testFrameworks, "JUNIT"));
 
                 let clouds = Common.getClouds();
                 assert.ok(clouds.length, "No cloud platforms found");
-                assert.ok( valueExists(clouds, "OCI") );
+                assert.ok(valueExists(clouds, "OCI"));
         });
 
         // create OCI project
         test("Create GCN project - Create project", async () => {
-                
+
                 let projFolder = path.resolve(__dirname, '../../../out/test/temp-proj');
-                
+
                 // TODO: This function fails mid-run and partialy populated project is created but not deleted. Make sure `projFolder` is empty.
                 if (!fs.existsSync(projFolder)) {
                         fs.mkdirSync(projFolder, { recursive: true });
-                    }
+                }
                 await Common.writeProjectContents(options, new NodeFileHandler(vscode.Uri.file(projFolder)));
 
-                fs.rmdirSync(projFolder, {recursive:true});
+                fs.rmdirSync(projFolder, { recursive: true });
         });
 
 
--- gcn/src/test/suite/extension.ui-test.ts
+++ gcn/src/test/suite/extension.ui-test.ts
@@ -17,24 +17,24 @@ import * as assert from 'assert';
  * @param input 
  * @param timeout 
  */
-async function waitForQuickPick(input : InputBox, timeout : number) : Promise<string[] | undefined> {
-  let picks = await input.getQuickPicks();
-  let items : string [] = [];
-  while (timeout > 0) {
-    if (picks.length) {
-      for (let pick of picks) {
-        items.push( await pick.getLabel() );
-      }
-      break;
-    } else {
-      await new Promise( f => setTimeout(f, 1000));
-      picks = await input.getQuickPicks();
-    }
+async function waitForQuickPick(input: InputBox, timeout: number): Promise<string[] | undefined> {
+    let picks = await input.getQuickPicks();
+    let items: string[] = [];
+    while (timeout > 0) {
+        if (picks.length) {
+            for (let pick of picks) {
+                items.push(await pick.getLabel());
+            }
+            break;
+        } else {
+            await new Promise(f => setTimeout(f, 1000));
+            picks = await input.getQuickPicks();
+        }
 
-    timeout -= 1;
-    if (timeout <= 0) return undefined;
-  }
-  return items;
+        timeout -= 1;
+        if (timeout <= 0) return undefined;
+    }
+    return items;
 }
 
 /**
@@ -43,94 +43,102 @@ async function waitForQuickPick(input : InputBox, timeout : number) : Promise<st
  * @param timeout 
  * @returns 
  */
-async function getInputText(input : InputBox, timeout : number) : Promise<string | undefined> {
-  while (timeout > 0) {
-    let inp = input.getText();
-    if (!inp) {
-      await new Promise( f => setTimeout(f, 1000));
-      timeout -= 1;
-      continue;
-    } else {
-      return inp;
+async function getInputText(input: InputBox, timeout: number): Promise<string | undefined> {
+    while (timeout > 0) {
+        let inp = input.getText();
+        if (!inp) {
+            await new Promise(f => setTimeout(f, 1000));
+            timeout -= 1;
+            continue;
+        } else {
+            return inp;
+        }
     }
-  }
-  return undefined;
+    return undefined;
 }
 
 // Create a Mocha suite
-describe('Extension UI tests', function() {
-	this.timeout(30000);
-	this.beforeAll(async () => {
-	        //await vscode.extensions.getExtension('oracle-labs-graalvm.gcn')?.activate();
-	});
-	// revert for tests
-	this.timeout(5000);
+describe('Extension UI tests', function () {
+    this.timeout(30000);
 
-  it("Create project", async () => {
-    // Open command pallet
-    await new Workbench().openCommandPrompt();
-    let input: InputBox = await InputBox.create();
+    it("Create project", async () => {
+        await new Promise(f => setTimeout(f, 20000));
 
-    // check if the command exists
-    await input.setText('> Graal Cloud Native: Create');
-    const picks = await input.getQuickPicks();
-    assert.ok(picks.length>0, "Command 'Create GCN Project' not found");
+        // Open command pallet
+        await new Workbench().openCommandPrompt();
+        let input: InputBox = await InputBox.create();
 
-    // select the command
-    await input.selectQuickPick(0);
+        // check if the command exists
+        await input.setText('> Graal Cloud Native: Create');
+        const picks = await input.getQuickPicks();
+        assert.ok(picks.length > 0, "Command 'Create GCN Project' not found");
 
-    // select micronaut version
-    const picks_micronaut : string[] | undefined = await waitForQuickPick(input, 10);
-    assert.ok(picks_micronaut !== undefined, "Microunaut quickpick failed to show");
-    assert.ok(picks_micronaut.length>0, "No micronaut versions available");
-    await input.selectQuickPick(0); // first version
+        // select the command
+        await input.selectQuickPick(0);
 
-    // select application type
-    let picks_app_type : string[] | undefined = await waitForQuickPick(input, 10);
-    assert.ok(picks_app_type !== undefined, "Application type quickpick failed to show");
-    assert.ok(picks_app_type.length>0, "No Application types available");
-    await input.selectQuickPick(0); // APPLICATION
- 
-    // select java version
-    let picks_java : string[] | undefined = await waitForQuickPick(input, 10);
-    assert.ok(picks_java !== undefined, "Java version quickpick failed to show");
-    assert.ok(picks_java.length>0, "No Java version available");
-    await input.selectQuickPick(0); // any installed java
+        // select micronaut version
+        const picks_micronaut: string[] | undefined = await waitForQuickPick(input, 10);
+        assert.ok(picks_micronaut !== undefined, "Microunaut quickpick failed to show");
+        assert.ok(picks_micronaut.length > 0, "No micronaut versions available");
+        await input.selectQuickPick(0); // first version
 
-    // get project name
-    let proj_name : string | undefined = await getInputText(input, 10);
-    assert.ok(proj_name !== undefined, "Project name input failed to show");
-    assert.strictEqual(proj_name, "demo", "Project name is not default");
-    await input.confirm();
+        await new Promise(f => setTimeout(f, 2000));
 
-    // get project name
-    let proj_package : string | undefined = await getInputText(input, 10);
-    assert.ok(proj_package !== undefined, "Project package input failed to show");
-    assert.strictEqual(proj_package, "com.example", "Project package is not default");
-    await input.confirm();
+        // select application type
+        let picks_app_type: string[] | undefined = await waitForQuickPick(input, 10);
+        assert.ok(picks_app_type !== undefined, "Application type quickpick failed to show");
+        assert.ok(picks_app_type.length > 0, "No Application types available");
+        await input.selectQuickPick(0); // APPLICATION
 
-    // pick project service
-    await new Promise( f => setTimeout(f, 1000));
-    await input.confirm();
+        // select java version
+        let picks_java: string[] | undefined = await waitForQuickPick(input, 10);
+        assert.ok(picks_java !== undefined, "Java version quickpick failed to show");
+        assert.ok(picks_java.length > 0, "No Java version available");
+        await input.selectQuickPick(0); // any installed java
 
-    // select build tool
-    let picks_build : string[] | undefined = await waitForQuickPick(input, 10);
-    assert.ok(picks_build !== undefined, "Build tool quickpick failed to show");
-    assert.ok(picks_build.length>0, "No build tool available");
-    await input.selectQuickPick(0); // Gradle Groovy
+        // get project name
+        let proj_name: string | undefined = await getInputText(input, 10);
+        assert.ok(proj_name !== undefined, "Project name input failed to show");
+        assert.strictEqual(proj_name, "demo", "Project name is not default");
+        await input.confirm();
 
-    // select test framework
-    let picks_framework : string[] | undefined = await waitForQuickPick(input, 10);
-    assert.ok(picks_framework !== undefined, "Test framework quickpick failed to show");
-    assert.ok(picks_framework.length>0, "No test framework available");
-    await input.selectQuickPick(0); // JUNIT
+        await new Promise(f => setTimeout(f, 1000));
 
-    // cancel cloud pick
-    await new Promise( f => setTimeout(f, 1000));
-    try {
-      await input.cancel();
-    } catch {}
+        // get project name
+        let proj_package: string | undefined = await getInputText(input, 10);
+        assert.ok(proj_package !== undefined, "Project package input failed to show");
+        assert.strictEqual(proj_package, "com.example", "Project package is not default");
+        await input.confirm();
 
-  }).timeout(20000);
+        // pick project service
+        await new Promise(f => setTimeout(f, 1000));
+        await input.confirm();
+        await new Promise(f => setTimeout(f, 1000));
+
+        // pick project features
+        await new Promise(f => setTimeout(f, 1000));
+        await input.confirm();
+        await new Promise(f => setTimeout(f, 1000));
+
+        // select build tool
+        let picks_build: string[] | undefined = await waitForQuickPick(input, 10);
+        await new Promise(f => setTimeout(f, 5000));
+        assert.ok(picks_build !== undefined, "Build tool quickpick failed to show");
+        assert.ok(picks_build.length > 0, "No build tool available");
+        await input.selectQuickPick(0); // Gradle Groovy
+
+        // select test framework
+        let picks_framework: string[] | undefined = await waitForQuickPick(input, 10);
+        assert.ok(picks_framework !== undefined, "Test framework quickpick failed to show");
+        assert.ok(picks_framework.length > 0, "No test framework available");
+        await input.selectQuickPick(0); // JUNIT
+
+        // cancel cloud pick
+        await new Promise(f => setTimeout(f, 1000));
+        try {
+            await input.cancel();
+        } catch { }
+
+    }).timeout(60000);
 
 });
